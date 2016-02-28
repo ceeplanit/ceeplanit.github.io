8f0df4e6bb22238537b4e597be7b1058de9264e0
ceeplanit <KristinSchields@Kristins-MacBook-Pro.local>
2016-02-28 14:55:40 -0500

    Adding JavaScript
    Because why not?

---
+++ script.js
@@ -0,0 +1,40 @@
+window.onload = function(e) {
+
+function turtleFact() {
+	alert("Some turtles can longer than humans");
+}
+
+// turtleFact()
+
+function temptGang(gangName) {
+	alert(gangName + " " + "Come out and play");
+}
+
+// temptGang()
+
+function addNumbers(num1, num2) {
+	alert(num1+num2);
+}
+
+// addNumbers(10, 5)
+
+function square(number) {
+	return number * number;
+}
+
+function displaySquare(squaredNumber) {
+	console.log("Your number is" + " " + squaredNumber);
+}
+
+// displaySquare(square(3));
+
+// document.getElementById
+
+var pageNode = document.getElementsByTagName('body')[0];
+// pageNode.innerHTML = '<h1>Oh Noes!</h1> <p>I just changed the whole page!</p>'
+
+var title = document.getElementById('openTitle');
+title.innerHTML = "Thanks for Coming to Class";
+
+
+}
