baae54b508e45f5cfd66f28f365883add9bad0c7
ceeplanit <KristinSchields@Kristins-MacBook-Pro.local>
2016-02-28 14:59:46 -0500

    Revise Headline

--- script.js
+++ script.js
@@ -33,8 +33,8 @@ function displaySquare(squaredNumber) {
 var pageNode = document.getElementsByTagName('body')[0];
 // pageNode.innerHTML = '<h1>Oh Noes!</h1> <p>I just changed the whole page!</p>'
 
-var title = document.getElementById('openTitle');
-title.innerHTML = "Thanks for Coming to Class";
+// var title = document.getElementById('openTitle');
+// title.innerHTML = "Thanks for Coming to Class";
 
 
 }
